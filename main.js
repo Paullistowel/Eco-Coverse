806b30493e13e9724c380e7bcba482d5a9849779
richmondazadze <richmondazadze1313@gmail.com>
2024-12-16 07:57:54 -0600

    changes from richmond

--- main.js
+++ main.js
@@ -2,17 +2,13 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", (e) => {
+menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("open");
-
   const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute(
-    "class",
-    isOpen ? "ri-close-line" : "ri-menu-3-line"
-  );
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-3-line");
 });
 
-navLinks.addEventListener("click", (e) => {
+navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-3-line");
 });
@@ -23,74 +19,37 @@ const swiper = new Swiper(".swiper", {
   loop: true,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  speed: 600,
 });
 
 const scrollRevealOption = {
-  distance: "50px",
+  distance: "30px",
   origin: "bottom",
-  duration: 1000,
+  duration: 800,
+  easing: "ease-in-out",
 };
 
-// header container
-ScrollReveal().reveal(".header__image img", {
-  ...scrollRevealOption,
-});
+ScrollReveal().reveal(".header__image img", scrollRevealOption);
+ScrollReveal().reveal(".header__content h4", { ...scrollRevealOption, delay: 300 });
+ScrollReveal().reveal(".header__content h1", { ...scrollRevealOption, delay: 600 });
+ScrollReveal().reveal(".header__content p", { ...scrollRevealOption, delay: 900 });
+ScrollReveal().reveal(".header__content .btn", { ...scrollRevealOption, delay: 1200 });
 
-ScrollReveal().reveal(".header__content h4", {
-  ...scrollRevealOption,
-  delay: 500,
-});
+ScrollReveal().reveal(".about__image img", { ...scrollRevealOption, origin: "left" });
+ScrollReveal().reveal(".about__content .section__header", { ...scrollRevealOption, delay: 300 });
+ScrollReveal().reveal(".about__content p", { ...scrollRevealOption, delay: 600 });
+ScrollReveal().reveal(".about__content h4", { ...scrollRevealOption, delay: 900 });
+ScrollReveal().reveal(".about__btns", { ...scrollRevealOption, delay: 1200 });
 
-ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-
-ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-
-ScrollReveal().reveal(".header__content .btn", {
-  ...scrollRevealOption,
-  delay: 2000,
-});
-
-// about container
-ScrollReveal().reveal(".about__image img", {
-  ...scrollRevealOption,
-  origin: "left",
-});
-
-ScrollReveal().reveal(".about__content .section__header", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-
-ScrollReveal().reveal(".about__content p", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-
-ScrollReveal().reveal(".about__content h4", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-
-ScrollReveal().reveal(".about__btns", {
-  ...scrollRevealOption,
-  delay: 2000,
-});
-
-// service container
 ScrollReveal().reveal(".service__card", {
-  duration: 1000,
-  interval: 500,
+  duration: 800,
+  interval: 300,
 });
 
-// blog container
-ScrollReveal().reveal(".blog__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
+ScrollReveal().reveal(".blog__card", { ...scrollRevealOption, interval: 300 });
